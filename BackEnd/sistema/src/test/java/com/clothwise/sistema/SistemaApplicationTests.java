71ac45a1a4f16677e70e04898a89ae41cfbed3d7
RodrigoMoron <rodrigomp.3008@gmail.com>
2026-06-08 21:23:21 -0500

    Separacion de lineas de trabajo simultaneas

---
+++ BackEnd/sistema/src/test/java/com/clothwise/sistema/SistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clothwise.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
